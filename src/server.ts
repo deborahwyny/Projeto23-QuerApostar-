2d4438a04d40b1f7fe9bfc5e35ad906936719868
Deborah Wyny <129215842+deborahwyny@users.noreply.github.com>
2023-11-14 15:44:07 -0300

    process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 4000;
+const port: number = Number(process.env.PORT) || 4000;
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
